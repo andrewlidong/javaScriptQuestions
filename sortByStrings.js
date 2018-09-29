373664c2112d3fe41c24ca89d9f23a5b07499d93
Andrew Dong <andrewdong1994@gmail.com>
2018-09-29 17:59:06 -0400

    sortByStrings

---
+++ sortByStrings.js
@@ -0,0 +1,7 @@
+// Sort the letters in a string s by the order they occur in the string t.
+// You can assume that t will not have repetitive characters.
+// For s = "weather" and t = "therapw" the output should be sortByString(s,t) = "theraw".  For s = "weaether" and t = "odg", the output should be sortByString(s, t) = "oodg"
+
+function sortByStrings(s,t) {
+  
+}
